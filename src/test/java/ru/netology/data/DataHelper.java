9a6f655fd2e11ad2c3d58d45a38839723ae98fb9
Ася Тамонова <asyatamoniva@192.168.1.161>
2022-10-09 20:02:47 +0300

    Add some SQL

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,2 +1,45 @@
-package ru.netology.data;public class DataHelper {
+package ru.netology.data;
+
+import lombok.SneakyThrows;
+import lombok.Value;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataHelper {
+    private DataHelper() {}
+    private static QueryRunner runner = new QueryRunner();
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+        return new AuthInfo("petya", "123qwerty");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    @SneakyThrows
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+        var login = authInfo.getLogin();
+        var authId = "SELECT id FROM users WHERE login = ?";
+        var codeSQL = "SELECT code FROM auth_codes JOIN users ON auth_codes.user_id = ?";
+
+        try (var conn = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/app-deadline", "user", "pass")) {
+            var result = runner.query(conn, codeSQL, new ScalarHandler<String>());
+            return new VerificationCode;
+        }
+    }
 }
--- src/test/java/ru/netology/test/VerifyTest.java
+++ src/test/java/ru/netology/test/VerifyTest.java
@@ -1,2 +1,47 @@
-package ru.netology.test;public class VerifyTest {
+package ru.netology.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VerifyTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
+
+    @AfterEach
+    void cleanAuth(){
+
+    }
+
+    @AfterAll
+    void cleanBase(){
+
+    }
+
+    @Test
+    void shouldViewDashboard() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        assertEquals("http://localhost:9999/dashboard", currentUrl);
+    }
+
 }
